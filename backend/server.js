6ff8950b4fef7d0c5dcd8bf7d601a8686bc7c2d6
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-06-21 20:52:16 +0530

    feat(expense): register expense route in server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
+
 import userRouter from './routes/userRoute.js';
 import incomeRouter from './routes/incomeRoute.js';
+import expenseRouter from './routes/dashboardRoute.js'
+
+
 
 const app = express();
 const port = 4000;
@@ -18,7 +22,10 @@ connectDB();
 
 // ROUTES
 app.use("/api/user", userRouter);
-app.use("/api/income", incomeRouter)
+app.use("/api/income", incomeRouter);
+app.use("/api/expense", expenseRouter);
+
+
 app.get('/',(req,res)=>{
     res.send("API Working");
 })
