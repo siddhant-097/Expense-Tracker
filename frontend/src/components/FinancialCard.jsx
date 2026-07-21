f85ef345403e08d59e6c29ddac94995f319b6bd9
siddhant-097 <siddhantchaudhary097@gmail.com>
2026-07-21 17:53:36 +0530

    feat: implement Dashboard component with financial metrics and transaction management

---
+++ frontend/src/components/FinancialCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const FinancialCard = ({
+    icon,
+    label,
+    value,
+    additionContent,
+    borderColor = " ",
+    bgColor = "bg-white",
+}) => (
+    <div className={`${bgColor} rounded-xl p-5 lg:mx-2 lg:p-2 shadow-sm
+     border hover:shadow-md border-gray-100 transition-all ${borderColor}`}
+    >
+      <div className={"text-sm font-medium text-gray-600 flex items-center gap-2"}>
+        {icon}
+        {label}
+      </div>
+      <p className="text-2xl font-bold text-gray-800 mt-1">
+        {value}
+      </p>
+      {additionContent}
+    </div>
+)
+
+export default FinancialCard
